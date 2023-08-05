e6b1e3c22ce166f69babe18d63fd159ec861e94e
Ojohn09 <141437041+Ojohn09@users.noreply.github.com>
2023-08-05 12:43:59 +0100

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element='' />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
