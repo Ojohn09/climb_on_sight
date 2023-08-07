90a433058fc5020687b88481ab92bfe9af551f11
Ojohn09 <141437041+Ojohn09@users.noreply.github.com>
2023-08-07 15:48:12 +0100

    new adjustment to the login page

--- src/auth/Login_views.jsx
+++ src/auth/Login_views.jsx
@@ -5,18 +5,15 @@ import back from '../assets/images/background.svg'
 
 function Login_views() {
     return (
-        <div className='max-w-[1920px] bg-[#FFF1E7] mx-auto h-full'>
-            <div className=' flex gap-4 p-9'>
+        <div className='max-w-[1920px] h-screen bg-[#FFF1E7] mx-auto'>
+            <div className=' flex gap-4 p-4'>
                 <div className='lg:w-[35%]'>
                     <img src={back} alt="" className='rounded-xl' />
                 </div>
-                <div className='bg-white lg:w-[65%] min-h-[70vh] p-2 rounded-xl flex flex-col items-center'>
+                <div className='bg-white lg:w-[65%] rounded-xl flex flex-col items-center'>
                     <LoginField_comp />
-                    <div>
-                        <p className='hover:underline cursor-pointer'>Don't have an account?</p>
-                    </div>
                 </div>
-               
+
 
             </div>
         </div>
